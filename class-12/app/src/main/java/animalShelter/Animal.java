4a094b87de1a1c5103a18ba9a61c5766cddaba90
aws69 <awsalomari11469@gmail.com>
2023-09-11 14:16:01 +0300

    class12

--- class-12/app/src/main/java/animalShelter/Animal.java
+++ class-12/app/src/main/java/animalShelter/Animal.java
@@ -1,27 +1,21 @@
 package animalShelter;
-
 class Animal {
     private final String species;
     private final String name;
     private int order;
-
     public Animal(String species, String name) {
         this.species = species;
         this.name = name;
     }
-
     public String getSpecies() {
         return species;
     }
-
     public String getName() {
         return name;
     }
-
     public int getOrder() {
         return order;
     }
-
     public void setOrder(int order) {
         this.order = order;
     }
